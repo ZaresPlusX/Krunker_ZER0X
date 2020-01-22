3b91fe63ce88e2647fa7569f58795d4c91a991c5
ZX <57325566+ZaresPlusX@users.noreply.github.com>
2020-01-22 12:35:49 +0200

    Create featured settings.js

---
+++ ᴇxᴛ/featured settings.js
@@ -0,0 +1,26 @@
+// ESP hack code
+let consts = { 'cameraHeight': 1.5, 'playerHeight': 0xb, 'cameraHeight': 1.5, 'headScale': 0x2, 'armScale': 1.3, 'armInset': 0.1, 'chestWidth': 2.6, 'hitBoxPad': 0x1, 'crouchDst': 0x3, 'camChaseTrn': 0.0022, 'camChaseSpd': 0.0012, 'camChaseSen': 0.2, 'camChaseDst': 0x18, 'recoilMlt': 0.3, 'nameOffset': 0.6, 'nameOffsetHat': 0.8 };
+const Pi = Math['PI'] / 0x2;
+const PI2 = 0x2 * Math['PI'];
+const defined = _0x3936dd => typeof _0x3936dd !== _0x3700('0x11') && _0x3936dd;
+let waterMark = _0x3aedb6 => {
+    if (aHolder) {
+        const _0x1a04a6 = aHolder['offsetParent'];
+        if (_0x1a04a6) {
+            if (_0x3aedb6 && !_0x1a04a6[_0x3700('0x12')]) {
+                const _0xf4e600 = document['createElement']('P');
+                _0xf4e600[_0x3700('0x13')] = _0x3700('0x14');
+                _0x1a04a6[_0x3700('0x15')](_0xf4e600);
+                _0x1a04a6[_0x3700('0x12')] = _0xf4e600;
+                _0x1a04a6[_0x3700('0x12')][_0x3700('0x16')](_0x3700('0x17'), _0x3700('0x18'));
+            } else if (!_0x3aedb6 && _0x1a04a6[_0x3700('0x12')]) {
+                _0x1a04a6[_0x3700('0x12')][_0x3700('0x19')]();
+                _0x1a04a6[_0x3700('0x12')] = null;
+            }
+        }
+    }
+};
+// GUI
+GUI = document[_0x3700('0x0')](_0x3700('0x1'));
+
+
