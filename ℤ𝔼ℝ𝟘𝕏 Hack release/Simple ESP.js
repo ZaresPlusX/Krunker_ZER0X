2cb49a96894c824063c8ff780e52f72c755fec9e
ZX <57325566+ZaresPlusX@users.noreply.github.com>
2020-02-10 12:34:45 +0200

    1.9.9 tampermonkey ESP hack

--- ℤ𝔼ℝ𝟘𝕏 Hack release/Simple ESP.js
+++ ℤ𝔼ℝ𝟘𝕏 Hack release/Simple ESP.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Krunker.io ESP
-// @namespace    https://github.com/ZaresPlusX
+// @namespace    https://github.com/ZaresPlusX/Krunker_ZER0X
 // @version      1.9.9
 // @description  Lmao
 // @author       ZaresPlus X
@@ -9,29 +9,13 @@
 // @grant        none
 // ==/UserScript==
 
-// Layout just :D
-(function(){
-    var original_push = Array.prototype.push;
-    function hook_push(...args) {
-        original_push.apply(this, args);
-        if (args[0] && args[0].armMeshes) {
-            Object.defineProperty(args[0], "lhYWIWew", { get: function() { return true } });
+// Just for fun
+        function cTX() {
+            if (!cDR['singlePlayer'] && (cDT && cDT['active'] || spectating))
+                for (var cCd = 0x0; cCd < cDR['players']['list']['length']; ++cCd)
+                    (cDV = cDR['players']['list'][cCd])['active'] && cDV['NurukoGiQHIcRRwIQZbAkAvXTNeJQDgY'] && cDV != cDT && (cDV['vYRDHNtoczWxPcpbhghRyYnBdehThUaq'] = !0x0,
+                    !spectating && !(cEn && cEn == cDV['team']) && !(cEn && cEn != cDV['team'] && cDR['mode']['teamSee'] && cDR['mode']['teamSee'][0x0] == cEn && cDR['mode']['teamSee'][0x1] == cDV['team']) && (cDR['config']['nameTags'] || cDR['mode']['hideNames'] || null != cDR['oMrVwggATjTbztEKMbtPRvJVGIdzONwA'](cDT, cDV['x'], cDV['y'], cDV['z'])) && (#8FCC2CcDV['vYRDHNtoczWxPcpbhghRyYnBdehThUaq'] = !0x0),
+                    (0x1 == cH1['hideNames']['val'] && cEn && cEn != cDV['team'] || 0x2 == cH1['hideNames']['val'] && cEn && cEn == cDV['team'] || 0x3 == cH1['hideNames']['val']) && (cDV['vYRDHNtoczWxPcpbhghRyYnBdehThUaq'] = !0x1),
+                    cDR['players']['toggleLOD'](cDV, null == cDR['oMrVwggATjTbztEKMbtPRvJVGIdzONwA'](cDK['camera']['jWEJolglnzvuolgaxrWLPzzkGgimFSLP'](), cDV['x'], cDV['y'], cDV['z'], 0xa)));
         }
-    }
-
-    var string_map = {};
-	var original_toString = Function.prototype.toString;
-	function hook_toString(...args) {
-		var return_string = original_toString.apply(this, args);
-		if (string_map[return_string]) {
-			return_string = string_map[return_string];
-		}
-		return return_string;
-	}
-
-	string_map[hook_push.toString()] = original_push.toString();
-	string_map[hook_toString.toString()] = original_toString.toString();
-
-    Array.prototype.push = hook_push;
-	Function.prototype.toString = hook_toString;
 })()
