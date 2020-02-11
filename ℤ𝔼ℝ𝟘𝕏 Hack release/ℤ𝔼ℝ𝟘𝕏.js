13f3d8de67fcf4e6ff1d177f8a8da3643cfd2659
ZX <57325566+ZaresPlusX@users.noreply.github.com>
2020-02-11 10:48:51 +0200

    build version

--- ℤ𝔼ℝ𝟘𝕏 Hack release/ℤ𝔼ℝ𝟘𝕏.js
+++ ℤ𝔼ℝ𝟘𝕏 Hack release/ℤ𝔼ℝ𝟘𝕏.js
@@ -17,3 +17,26 @@
             value: value
         });
     };
+
+    let conceal_function = function(original_Function, hook_Function) {
+        shared_state.get('functions_to_hide').set(hook_Function, original_Function);
+    };
+//diff
+    let conceal_string = function(original_string, hook_string) {
+        shared_state.get('strings_to_hide').push({
+            from: new RegExp(hook_string.replace(/([\[|\]|\(|\)|\*|\\|\.|\+])/g, '\\$1'), 'g'),
+            to: original_string
+        });
+    };
+//diff
+    const original_toString = Function.prototype.toString;
+    let hook_toString = new Proxy(original_toString, {
+        apply: function(target, _this, _arguments) {
+            //diff
+            try {
+                var ret = Function.prototype.apply.apply(target, [_this, _arguments]);
+            } catch (e) {
+                // modify stack trace to hide proxy
+                e.stack = e.stack.replace(/\n.*Object\.apply \(<.*/, '');
+                throw e;
+            }
