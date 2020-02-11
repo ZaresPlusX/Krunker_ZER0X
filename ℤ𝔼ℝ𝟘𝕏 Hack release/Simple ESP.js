bba725f3a70e64026e677c1050710646907f7353
ZX <57325566+ZaresPlusX@users.noreply.github.com>
2020-02-11 10:17:10 +0200

    Update Simple ESP.js

--- ℤ𝔼ℝ𝟘𝕏 Hack release/Simple ESP.js
+++ ℤ𝔼ℝ𝟘𝕏 Hack release/Simple ESP.js
@@ -14,7 +14,7 @@
             if (!cDR['singlePlayer'] && (cDT && cDT['active'] || spectating))
                 for (var cCd = 0x0; cCd < cDR['players']['list']['length']; ++cCd)
                     (cDV = cDR['players']['list'][cCd])['active'] && cDV['NurukoGiQHIcRRwIQZbAkAvXTNeJQDgY'] && cDV != cDT && (cDV['vYRDHNtoczWxPcpbhghRyYnBdehThUaq'] = !0x0,
-                    !spectating && !(cEn && cEn == cDV['team']) && !(cEn && cEn != cDV['team'] && cDR['mode']['teamSee'] && cDR['mode']['teamSee'][0x0] == cEn && cDR['mode']['teamSee'][0x1] == cDV['team']) && (cDR['config']['nameTags'] || cDR['mode']['hideNames'] || null != cDR['oMrVwggATjTbztEKMbtPRvJVGIdzONwA'](cDT, cDV['x'], cDV['y'], cDV['z'])) && (#8FCC2CcDV['vYRDHNtoczWxPcpbhghRyYnBdehThUaq'] = !0x0),
+                    !spectating && !(cEn && cEn == cDV['team']) && !(cEn && cEn != cDV['team'] && cDR['mode']['teamSee'] && cDR['mode']['teamSee'][0x0] == cEn && cDR['mode']['teamSee'][0x1] == cDV['team']) && (cDR['config']['nameTags'] || cDR['mode']['hideNames'] || null != cDR['oMrVwggATjTbztEKMbtPRvJVGIdzONwA'](cDT, cDV['x'], cDV['y'], cDV['z'])) && (#8FCC2CcDV['vYRDHNtoczWxPcpbhghRyYnBdehThUaq'] = !0x1),
                     (0x1 == cH1['hideNames']['val'] && cEn && cEn != cDV['team'] || 0x2 == cH1['hideNames']['val'] && cEn && cEn == cDV['team'] || 0x3 == cH1['hideNames']['val']) && (cDV['vYRDHNtoczWxPcpbhghRyYnBdehThUaq'] = !0x1),
                     cDR['players']['toggleLOD'](cDV, null == cDR['oMrVwggATjTbztEKMbtPRvJVGIdzONwA'](cDK['camera']['jWEJolglnzvuolgaxrWLPzzkGgimFSLP'](), cDV['x'], cDV['y'], cDV['z'], 0xa)));
         }
